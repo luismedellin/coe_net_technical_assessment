03c92147f325fe0b81007abd8dbd15f58c066b0b
Luis Velandia <luiseduardo1218@gmail.com>
2026-01-13 17:46:35 -0500

    Get products from back to front

--- code/backend/TA-API/Program.cs
+++ code/backend/TA-API/Program.cs
@@ -12,6 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console());
     builder.Services.AddDbContext<AssessmentDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("AssessmentDB")));
 
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("FrontEndPolicy", policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:1234")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    });
+
     // Swagger
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
@@ -29,6 +41,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     app.UseSerilogRequestLogging();
+
+    app.UseCors("FrontEndPolicy");
 
     if (app.Environment.IsDevelopment())
     {
--- code/frontend/react/src/app/pages/Home/index.jsx
+++ code/frontend/react/src/app/pages/Home/index.jsx
@@ -1,4 +1,22 @@
-export function HomePage() {
+import { useEffect, useState } from "react";
+
+export const HomePage = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fechProducts = async () => {
+      try {
+        const response = await fetch("https://localhost:7189/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fechProducts();
+  }, []);
 
   return (
     <div className="row">
